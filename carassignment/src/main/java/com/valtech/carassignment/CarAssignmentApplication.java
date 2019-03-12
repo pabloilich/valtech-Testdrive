15a058741c730e0b54302e0fff673d595d143eac
pabloilich-grupoassa <pilich@grupoassa.com>
2019-03-12 15:48:41 -0300

    First Commit
    FC

---
+++ carassignment/src/main/java/com/valtech/carassignment/CarAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.valtech.carassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarAssignmentApplication.class, args);
+	}
+
+}
